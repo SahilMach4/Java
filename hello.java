22b487db989afd8c17620862b220c20c92d8e163
SahilMach4 <163117778+SahilMach4@users.noreply.github.com>
2024-03-12 16:02:23 +0530

    Add hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println(x);
+        System.out.println("Hello, World");
+        System.out.println("Goodbye, World");
+    }
+}
